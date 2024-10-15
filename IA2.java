5449d26b4306b635429053bd1d0f3145229dce12
Harshitha1016 <harshitha.23cs045@sode-edu.in>
2024-10-15 15:39:02 +0530

    USN

---
+++ IA2.java
@@ -0,0 +1,17 @@
+package pkg1;
+import java.util.Scanner;
+public class IA2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the starting usn:");
+        int number = scanner.nextInt();
+        System.out.println("Enter the ending usn:");
+        int number1 = scanner.nextInt();
+        System.out.println("USN are:");
+        String usn2="";
+        for (int i = number; i <= number1; i++) {
+        	usn2=String.format("%03d",i);
+            System.out.println("4MW23CS"+usn2);
+        }
+    }
+}
