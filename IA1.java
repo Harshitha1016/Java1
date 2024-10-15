857ea8887446b3f0787ebd2efd3d470b241a0874
Harshitha1016 <harshitha.23cs045@sode-edu.in>
2024-10-15 15:38:23 +0530

    Celcius to Fahrenheit

---
+++ IA1.java
@@ -0,0 +1,17 @@
+package pkg1;
+
+import java.util.Scanner;
+
+public class IA1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	    float tempC;
+		float tempF;
+		System.out.println("Enter the temperature in celcius:");3
+		0
+		tempC = scanner.nextFloat();
+		tempF=((tempC*9)/5)+32;
+		System.out.println("Temperature in Fahrenheit:"+tempF);
+	}
+
+}
