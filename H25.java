15c1c6c7b51f14196a7f073ff3329adc45a109ff
Harshitha1016 <harshitha.23cs045@sode-edu.in>
2024-12-03 15:16:54 +0530

    Outer Class

---
+++ H25.java
@@ -0,0 +1,20 @@
+//4
+package pkg1;
+class OuterClass {
+  String outerField = "Outer field";
+  static String staticOuterField = "Static outer field";
+  class InnerClass {
+      void accessMembers() {
+          System.out.println(outerField);
+          System.out.println(staticOuterField);
+      }
+  }
+}
+public class H25{
+	public static void main(String[] args) {
+      System.out.println("Inner class:");
+      OuterClass o1 =new OuterClass();
+      OuterClass.InnerClass i1 = o1.new InnerClass();
+      i1.accessMembers();
+  }
+}
